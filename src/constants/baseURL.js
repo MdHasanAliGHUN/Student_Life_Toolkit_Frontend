a459e4359dcf9a1c0c6b6c48d331ba1b2e132bd0
Md Hasan Mahmud <mdhassanali01947@gmail.com>
2025-09-10 08:40:36 +0600

    Update Base url

--- src/constants/baseURL.js
+++ src/constants/baseURL.js
@@ -1,3 +1,2 @@
-export const BASE_URL = "http://localhost:5000/api"
+export const BASE_URL = "https://student-life-toolkit-server.onrender.com/api/api"
 
-// export const BASE_URL = "https://student-life-toolkit-backend-fr79.vercel.app";
